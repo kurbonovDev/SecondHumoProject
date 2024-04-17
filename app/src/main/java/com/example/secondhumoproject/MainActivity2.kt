847080e9fee2b10cf008485a03c8cd7f1fbd68c6
Muhammad Kurbonov <muhammadkurbonov25@mail.ru>
2024-04-17 09:33:01 +0500

    rcView

--- app/src/main/java/com/example/secondhumoproject/MainActivity2.kt
+++ app/src/main/java/com/example/secondhumoproject/MainActivity2.kt
@@ -10,12 +10,13 @@ import com.example.secondhumoproject.databinding.ActivityMain2Binding
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
-    private var strongList = listOf("Smart","Strong")
-    private var weakList = listOf("Dumb","weak")
+    private var strongList = listOf("Smart","Strong","Smart","Strong","Smart","Strong","Smart","Strong")
+    private var weakList = listOf("Dumb","weak","Dumb","weak","Dumb","weak","Dumb","weak","Dumb","weak","Dumb","weak")
 
     private lateinit var rcViewStrong: RecyclerView
     private lateinit var rcViewWeak: RecyclerView
-    private lateinit var adapter: Adapter
+    private lateinit var adapterStrong: Adapter
+    private lateinit var adapterWeak: Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
@@ -26,21 +27,22 @@ class MainActivity2 : AppCompatActivity() {
     private fun initRcViewStrong(){
         rcViewStrong = binding.rcView1
         rcViewStrong.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        adapter= Adapter(strongList)
-        rcViewStrong.adapter=adapter
+        adapterStrong= Adapter(strongList)
+        rcViewStrong.adapter=adapterStrong
     }
 
     private fun initRcViewWeak(){
-        rcViewWeak = binding.rcView1
+        rcViewWeak = binding.rcView2
         rcViewWeak.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        adapter= Adapter(weakList,false)
-        rcViewWeak.adapter=adapter
+        adapterWeak= Adapter(weakList,false)
+        rcViewWeak.adapter=adapterWeak
     }
     override fun onResume() {
         super.onResume()
 
-        initRcViewStrong()
+
         initRcViewWeak()
+        initRcViewStrong()
 
 
 
