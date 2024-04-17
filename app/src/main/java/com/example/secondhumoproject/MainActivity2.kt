8d934cf60ed41562d0f017193070ced0dbf9a6e8
Muhammad Kurbonov <muhammadkurbonov25@mail.ru>
2024-04-17 08:57:08 +0500

    rcView

--- app/src/main/java/com/example/secondhumoproject/MainActivity2.kt
+++ app/src/main/java/com/example/secondhumoproject/MainActivity2.kt
@@ -2,10 +2,47 @@ package com.example.secondhumoproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.Adapter
+import com.example.secondhumoproject.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+    private var strongList = listOf("Smart","Strong")
+    private var weakList = listOf("Dumb","weak")
+
+    private lateinit var rcViewStrong: RecyclerView
+    private lateinit var rcViewWeak: RecyclerView
+    private lateinit var adapter: Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    private fun initRcViewStrong(){
+        rcViewStrong = binding.rcView1
+        rcViewStrong.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        adapter=Adapter(strongList)
+        rcViewStrong.adapter=adapter
+    }
+
+    private fun initRcViewWeak(){
+        rcViewWeak = binding.rcView1
+        rcViewWeak.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        adapter=Adapter(weakList,false)
+        rcViewWeak.adapter=adapter
+    }
+    override fun onResume() {
+        super.onResume()
+
+        initRcViewStrong()
+        initRcViewWeak()
+
+
+
     }
 }
